e6a1e05304fdd7c1083038787e8e55856319aec5
devCalf <caique.rosa@epiousion.com.br>
2024-10-14 11:57:34 -0300

    ajuste vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import LoginView from '../views/LoginView.vue'; 
 
 const routes = [
@@ -22,7 +22,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('/frontend_spider/'),
+  history: createWebHashHistory('/frontend_spider/'),
   routes,
 });
 
